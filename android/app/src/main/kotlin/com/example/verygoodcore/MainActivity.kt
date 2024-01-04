88b8e440abcbc1a7b0f44a9658c01a8c82f9ed95
rhowelp <rhowelp@gmail.com>
2024-01-05 07:52:49 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.fundsflow_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:fundsflow_app/app/app.dart';
+import 'package:fundsflow_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
